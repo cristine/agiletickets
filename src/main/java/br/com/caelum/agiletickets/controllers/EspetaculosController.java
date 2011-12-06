5f98cab796064cfb7bc5986ebe803cc39d339008
cristine <cristinedantas@gmail.com>
2011-12-06 09:20:20 -0200

    metodos nao utilizados no controller

--- src/main/java/br/com/caelum/agiletickets/controllers/EspetaculosController.java
+++ src/main/java/br/com/caelum/agiletickets/controllers/EspetaculosController.java
@@ -29,7 +29,7 @@ public class EspetaculosController {
 	private final Agenda agenda;
 	private Validator validator;
 	private Result result;
-	private Estabelecimento estabelecimento;
+
 
 	private final DiretorioDeEstabelecimentos estabelecimentos;
 
@@ -124,7 +124,5 @@ public class EspetaculosController {
 		return espetaculo;
 	}
 
-	private Estabelecimento criaEstabelecimento(Long id) {
-		return estabelecimentos.todos().get(0);
-	}
+
 }
